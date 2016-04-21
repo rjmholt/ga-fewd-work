f72dcc1209609ed0bf0bd25e86d84afc5f31c159
Robert Holt <robert.holt@posmc.com.au>
2016-04-21 22:47:01 +1000

    Added some pages to the project. Added some week 7 materials

--- W6/color_scheme/js/index.js
+++ W6/color_scheme/js/index.js
@@ -1,20 +1,7 @@
-$('#grayButton').click(switchGray);
-$('#whiteButton').click(switchWhite);
-$('#blueButton').click(switchBlue);
-$('#yellowButton').click(switchYellow);
-
-function switchGray() {
-  $('body').attr('class', 'gray');
-}
-
-function switchWhite() {
-  $('body').attr('class', 'white');
-}
-
-function switchBlue() {
-  $('body').attr('class', 'blue');
-}
-
-function switchYellow() {
-  $('body').attr('class', 'yellow');
-}
+$(document).ready(function () {
+    $('#grayButton, #whiteButton, #blueButton, #yellowButton').on('click',
+        function () {
+            var colour = $(this).attr('id').replace('Button', '');
+            $('body').attr('class', colour);
+        });
+});
