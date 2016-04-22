b0af10c17fe50ee49bd6ae87742336fa3af2ddec
Robert Holt <robert.holt@posmc.com.au>
2016-04-22 17:59:07 +1000

    Added webserver

---
+++ FinalProject/robnet/_rel/robnet_release/lib/robnet-0.0.1/priv/assets/contact.hs
@@ -0,0 +1,14 @@
+import Data.Char
+import Data.Bits
+import Control.Monad.State.Lazy
+
+s = "rjmholt@gmail.com"
+iv = 0x21817
+
+e (c:cs) = do
+  k <- get
+  let k' = k `xor` c
+  put k'
+  ks <- e cs
+  return (k:ks)
+
---
+++ FinalProject/robnet/src/robnet_app.erl
@@ -0,0 +1,22 @@
+-module(robnet_app).
+-behaviour(application).
+
+-export([start/2]).
+-export([stop/1]).
+
+start(_Type, _Args) ->
+    Paths =
+    [
+     {"/", cowboy_static, {priv_file, robnet, "assets/index.html"}},
+     {"/[...]", cowboy_static, {priv_dir, robnet, "assets"}}
+    ],
+    Dispatch = cowboy_router:compile([
+        {'_', Paths}
+    ]),
+    cowboy:start_http(my_http_listener, 100, [{port, 8080}],
+                      [{env, [{dispatch, Dispatch}]}]
+                     ),
+    robnet_sup:start_link().
+
+stop(_State) ->
+    ok.
